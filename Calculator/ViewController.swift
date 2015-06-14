f74dad1c90d0f8bc98e5c3c4108d1cc9d7855e67
井上航 <k.prfm19@gmail.com>
2015-06-15 01:12:35 +0900

    complete

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var num:Int = 0
-    var st: Int = 0
+    var num:Float = 0
+    var st: Float = 0
     var operation:Int = 0
+    var floatdisplay:Bool = false //小数表示するか否か true -> する
     @IBOutlet weak var mainlabel: UILabel!
 
     override func viewDidLoad() {
@@ -26,15 +27,16 @@ class ViewController: UIViewController {
     }
     
     //番号ボタンが押された時のlabel更新
-    private func renewal(btnum:Int){
+    private func renewal(btnum:Float){
+        //演算子を選択していないとエラーメッセージを表示
         if operation == 5 {
-            mainlabel.text = String("\(num) (ERR:Choice ope)")
+            mainlabel.text = String(format: "%.f (ERR:Choice Ope)",num)
         }else{
             if st != 0 {
                 st = st * 10
             }
             st += btnum
-            mainlabel.text = String(st)
+            mainlabel.text = String(format: "%.f", st)
         }
     }
     
@@ -46,45 +48,74 @@ class ViewController: UIViewController {
             num += st
         } else if operation == 2 {
             num -= st
+        } else if operation == 3 {
+            num *= st
         } else if operation == 5 {
             //do nothing 次は必ず演算子
         }
     }
-    
-    
+    //演算子ボタンが押された時の動き
+    private func pressope(x:Int){
+        if operation != 5 {
+            clc()
+        }
+        st = 0
+        operation = x
+    }
+    @IBAction func bt0() {
+        renewal(0)
+    }
+    @IBAction func bt1() {
+        renewal(1)
+    }
+    @IBAction func bt2() {
+        renewal(2)
+    }
     @IBAction func bt3() {
         renewal(3)
     }
     @IBAction func bt4() {
         renewal(4)
     }
+    @IBAction func bt5() {
+        renewal(5)
+    }
+    @IBAction func bt6() {
+        renewal(6)
+    }
+    @IBAction func bt7() {
+        renewal(7)
+    }
+    @IBAction func bt8() {
+        renewal(8)
+    }
+    @IBAction func bt9() {
+        renewal(9)
+    }
     
-    @IBAction func plus() { // tag -> 1
-        if operation != 5 {
-            clc()
-        }
-        mainlabel.text = String("\(num) +")
-        st = 0
-        operation = 1
+    @IBAction func plus() {
+        pressope(1)
+        mainlabel.text = String(format: "%.f +", num)
     }
     @IBAction func minus() {
-        if operation != 5 {
-            clc()
-        }
-        mainlabel.text = String("\(num) -")
-        st = 0
-        operation = 2
+        pressope(2)
+        mainlabel.text = String(format: "%.f -", num)
+    }
+    @IBAction func multiply() {
+        pressope(3)
+        mainlabel.text = String(format: "%.f ×", num)
+
     }
     @IBAction func btac() {
         st = 0
         num = 0
         operation = 0 //初期化
-        mainlabel.text = String(num)
+        mainlabel.text = String(0)
     }
     @IBAction func btclc() { //「=」ボタン
         clc()
         operation = 5 //追加演算の準備
-        mainlabel.text = String("= \(num)")
+        mainlabel.text = String(format: "= %.f", num)
         
     }
 
